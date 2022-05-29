bd81613ffbd88d9f93e908ff59a00713cd88837c
Oliver Scharkowski <59687448+oscharko@users.noreply.github.com>
2022-05-29 13:42:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+let head = [1, 2, 3, 4, 5]
+
+const middleNodeFirstSolution = function(head) {
+  if (head.length % 2 === 0) {
+    return head.splice((head.length / 2), head.length)
+  } else {
+    return head.splice(((head.length - 1) / 2), head.length)
+  }
+};
+
+const middleNodeSecondSolution = function(head) {
+  let headNew = []
+  if (head.length % 2 === 0) {
+    for (let i = (head.length / 2); i < head.length; i++) {
+      headNew.push(head[i])
+    }
+  } else {
+    for (let i = ((head.length - 1) / 2); i < head.length; i++) {
+      headNew.push(head[i])
+    }
+  }
+  return headNew
+};
+
+const middleNode = function(head) {
+  let node = head;
+  let length = 0;
+  while (node != null) {
+    length++;
+    node = node.next;
+  }
+  for (let i = 0; i < parseInt(length / 2); i++) {
+    head = head.next;
+  }
+  return head;
+};
+
+console.log(middleNodeSecondSolution(head))
